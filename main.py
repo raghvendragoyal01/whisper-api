1023462a6b19a7e85a1f3fea59b9b8410f3c4189
raghvendragoyal01 <149790025+raghvendragoyal01@users.noreply.github.com>
2025-11-13 22:17:29 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,36 @@
+from fastapi import FastAPI, UploadFile, File
+import whisper
+import uvicorn
+import os
+import uuid
+
+app = FastAPI()
+
+UPLOAD_FOLDER = "uploads"
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
+model = whisper.load_model("medium.en")
+
+@app.get("/")
+def home():
+    return {"message": "Whisper API is running!"}
+
+@app.post("/transcribe")
+async def transcribe_audio(file: UploadFile = File(...)):
+    ext = file.filename.split(".")[-1]
+    filename = f"{uuid.uuid4()}.{ext}"
+    file_path = os.path.join(UPLOAD_FOLDER, filename)
+
+    with open(file_path, "wb") as f:
+        f.write(await file.read())
+
+    result = model.transcribe(file_path)
+
+    return {
+        "text": result["text"],
+        "saved_file": file_path
+    }
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=10000)
